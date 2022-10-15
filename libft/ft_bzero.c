8785e694664aa564e7becc7172a1b50d8dcccb4e
Huseyin Cakmak <hucakmak@k1m42s06.42istanbul.com.tr>
2022-10-15 10:28:57 +0300

    bzero-finish

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -3,9 +3,16 @@
 void ft_bzero(void *s, size_t n)
 
 {
-	char a[] = s;
-
-
-
+	char *a;
+	a = s;
+	size_t i = 0;
+	size_t len = 0;
+	while (a[len] != 0)
+	{
+		len++;
+	}
+	while (i < len && i < n)
+	{
+		a[i++] = 0;
+	}
 }
-
--- libft/libft.h
+++ libft/libft.h
@@ -13,6 +13,7 @@ void *ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 size_t ft_strlcat(char *dst, const char *src, size_t dstsize);
 void *ft_memmove(void *dst, const void *src, size_t len);
 size_t ft_strlcpy(char *dst, const char *src, size_t dstsize);
+void ft_bzero(void *s, size_t n);
 
 
 
