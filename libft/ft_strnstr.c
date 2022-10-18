c18c464a5a721c237b2b041edde0380db3a25c97
Huseyin Cakmak <hucakmak@k1m42s06.42istanbul.com.tr>
2022-10-18 19:13:17 +0300

    ft_strnstr.c-fixed

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -1,47 +1,56 @@
 #include "libft.h"
-#include "string.h"
 
-char *ft_strnstr(const char *haystack, const char *needle, size_t len)
-
+char kontrol(const char *a, const char *b)
 {
 	size_t i;
 	i = 0;
-	size_t j;
-
-	if(!needle || len == 0)
+	size_t len;
+	len = ft_strlen(b);
+	while (a[i] == b[i])
 	{
-		return (0);
-	}
-	while(haystack[i] != 0)
-	{
-		printf("1.while girildi.\n");
-		j = 0;
-		while (haystack[i] == needle[j] && needle[i] != 0)
-		{
-			printf("2.while girildi\n");
-			i++;
-			j++;
-			if(needle[j] == 0)
-			{
-				printf("if girildi\n");
-				return (char *)(haystack + i);
-			}
-
-			printf("J nin 2. while içi değeri: %zu\n",j);
-			printf("i nin 2. while içi değeri: %zu\n",i);
-
-		}
+		if(b[i] == b[len - 1])
+			return(1);
 		i++;
 	}
-	printf("i nin değeri: %zu\n",i);
-	printf("j nin değeri: %zu\n",j);
 	return (0);
 }
 
+char *ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t i;
+	i = 0;
+	char * empty = (char *)"";
+	if(needle == empty)
+	{
+		return (char *)(haystack);
+	}
+	if(len == 0)
+		return (0);
+
+	while (haystack[i] != 0 && i <= (len - ft_strlen(needle)))
+	{
+		while (haystack[i] == needle[0] && haystack[i] != 0)
+		{
+			if(kontrol((haystack + i),needle) == 1)
+			{
+				return (char *)(haystack + i);
+			}
+			i++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+/*
 int main()
 {
-	char bir[30] = "aaabcabcd";
-	char iki[10] = "aabc";
-	printf("Benim sonucum: %p\n",ft_strnstr(bir,iki,-1));
-	printf("sistem sonucu: %p\n",strnstr(bir,iki,-1));
+	char haystack[30] = "aaabcabcd";
+	char needle[10] = "aabc";
+	char * empty = (char*)"";
+
+	printf("benim sonuç: %p\n",ft_strnstr(haystack,"a",1));
+	printf("sistem sonuç: %p\n",strnstr(haystack,"a",1));
+
 }
+*/
