7c699f7fc46a288c0e2fe262cf44c5751b7aca25
Huseyin Cakmak <huseyincakmakpm@gmail.com>
2022-11-20 01:57:18 +0300

    sleep-time

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -3,16 +3,84 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hucakmak <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: hucakmak <hucakmak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 22:41:20 by hucakmak          #+#    #+#             */
-/*   Updated: 2022/11/18 22:41:27 by hucakmak         ###   ########.fr       */
+/*   Updated: 2022/11/20 00:10:56 by hucakmak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <string.h>
 #include "libft.h"
+
+
+// Kelime sayısı
+// kelime sayısı kadar çift pointerli dizinin içinde char * yer aç
+// her bir kelimenin lenini bulup yer acmak
+// sırasıyla çift pointlerli dizinin içindeki tek pointerli char * 'a yer aç
+//
+
+int konum_bul(char const *str, char c)
+{
+    while (c!)
+    {
+        /* code */
+    }
+    
+}
+
+int countWords(char const *str, char c)
+{
+    int state;
+    int wc;
+    int i;
+
+    state = 0;
+    wc = 0;
+    i = 0;
+    while (str[i])
+    {
+        if (str[i] == c)
+            state = 0;
+        else if (state == 0)
+        {
+            state = 1;
+            wc++;
+            
+        }
+        i++;
+    }
+    return wc;
+}
+
 
 char **ft_split(char const *s, char c)
 {
-
+    char **arr_2d;
+    char *tmp = strdup(s);
+    int cw;
+    int i;
+    int j;
+    
+    cw = countWords(s, c);
+    if (cw == 0)
+        return (0);
+    arr_2d = malloc(cw * sizeof(char *) + 1);
+    if (!arr_2d)
+        return (0);
+    i = 0;
+    j = 0;
+    while (i < cw)
+    {
+        while (tmp[j] != c && tmp[j] != 0)
+            j++;
+        tmp[j] = 0;
+        arr_2d[i] = strdup(tmp);
+        j++;
+        i++;
+    }
+    free(tmp);
+    arr_2d[cw] = 0;
+    return (arr_2d);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -28,6 +28,9 @@ int ft_atoi(const char *str);
 void *ft_calloc(size_t count, size_t size);
 char *ft_substr(char const *s, unsigned int start, size_t len);
 char *ft_strjoin(char const *s1, char const *s2);
+char *ft_strtrim(char const *s1, char const *set);
+char *ft_itoa(int n);
+char **ft_split(char const *s, char c);
 
 
 #endif
