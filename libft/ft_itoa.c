d77ba0ce826e0f8fb62270827f7f273853dc37cb
Huseyin Cakmak <hucakmak@k1m21s03.42istanbul.com.tr>
2022-12-07 19:35:56 +0300

    work-stop

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -1,6 +1,43 @@
 #include "libft.h"
 
-char *ft_itoa(int n7)
+char basamak(int n)
 {
+    int a;
+    a = 0;
+    if(n < 0 || n == 0)
+        a++;
+    while(n != 0)
+    {
+        n = n / 10;
+        a++;
+    }
+    return (a);
+}
 
+char *ft_itoa(int n)
+{
+    char *nbr;
+    int i;
+    int a;
+
+    i = 0;
+    a = basamak(n);
+    nbr = malloc(sizeof(char) * (a + 1));
+    if(!nbr)
+        return(0);
+    while(i < a)
+    {
+        if (n == -2147483648)
+        {
+            
+        }
+        else if (n < 0)
+        {
+          
+        }
+        else
+        {
+        
+        }
+    }   
 }
